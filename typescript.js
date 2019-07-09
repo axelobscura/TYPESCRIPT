6b0d049f38c47f350db7bb231174d5f0a29affab
Axel <axosar@gmail.com>
2019-07-09 17:58:00 -0500

    Basic Types
    Remember typescript is a superset of javascript

--- typescript.js
+++ typescript.js
@@ -1,2 +1,17 @@
 // boolean
 var isCool = true;
+// number
+var age = 56;
+//string
+var eyeColor = 'brown';
+var favouriteColor = "I'm not old, i'm only " + age;
+//Arrays
+var pets = ['cat', 'dog', 'pig'];
+var pets2 = ['lion', 'dragon', 'lizard'];
+//Objects
+var wizard = {
+    a: 'John'
+};
+//null and undefined
+var meh = undefined;
+var noo = null;
--- typescript.ts
+++ typescript.ts
@@ -1,2 +1,22 @@
 // boolean
 let isCool: boolean = true;
+
+// number
+let age: number = 56;
+
+//string
+let eyeColor: string = 'brown';
+let favouriteColor: string = `I'm not old, i'm only ${age}`;
+
+//Arrays
+let pets: string[] = ['cat', 'dog', 'pig'];
+let pets2: Array<string> = ['lion', 'dragon', 'lizard'];
+
+//Objects
+let wizard: object = {
+  a: 'John'
+}
+
+//null and undefined
+let meh: undefined = undefined;
+let noo: null = null;
